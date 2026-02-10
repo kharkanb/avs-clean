d51e079f0c9c38fc8fd29fa200a27eb0cda17a87
telecom-admin <masoudeh.mirhosseini@gmail.com>
2026-02-10 18:58:23 +0330

    رفع migration تکراری و اضافه کردن routes تجهیزات

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
-﻿<?php
+﻿use App\Http\Controllers\Api\EquipmentController;
 
-use Illuminate\Support\Facades\Route;
+Route::prefix('equipment')->group(function () {
+    Route::get('/', [EquipmentController::class, 'index']);
+    Route::post('/', [EquipmentController::class, 'store']);
+});
 use App\Http\Controllers\Api\EquipmentController;
 
-Route::get('/equipment', [EquipmentController::class, 'index']);
-Route::post('/equipment', [EquipmentController::class, 'store']);
+Route::get("/equipment", [EquipmentController::class, "index"]);
+Route::post("/equipment", [EquipmentController::class, "store"]);
+Route::get("/equipment/{equipment}", [EquipmentController::class, "show"]);
+Route::put("/equipment/{equipment}", [EquipmentController::class, "update"]);
+Route::delete("/equipment/{equipment}", [EquipmentController::class, "destroy"]);
